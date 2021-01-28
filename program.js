3cddb6117d3fa79e0865520f3c1fbf3688df2834
Phong Phú <phongphu.official@gmail.com>
2021-01-28 08:47:50 +0700

    lan01

--- program.js
+++ program.js
@@ -1,8 +1,56 @@
-// Add new functions, variables here
+function findPrimeNumber(naturalNumber) {
+  let isPrime = true;
+  if (naturalNumber === 1) {
+  }
+  else if (naturalNumber > 1) {
+    for (let i = 2; i < naturalNumber; i++) {
+      if (naturalNumber % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      return naturalNumber;
+    }
+  }
+  else {
+    return;
+  }
+}
+
+function result(naturalNumber) {
+  let resultArray = [];
+  let tempNaturalNumber = naturalNumber;
+
+  for (let i = 0; i <tempNaturalNumber; i++ ){
+    if(naturalNumber % findPrimeNumber(i) == 0){
+      resultArray.push(i);
+      naturalNumber = naturalNumber / i;
+      resultArray.push(naturalNumber);
+      console.log(resultArray);
+    }
+  }
+
+
+  // for (let i = naturalNumber; i == 2; i--) {
+  //   if (findPrimeNumber(i) % naturalNumber == 0) {
+  //     resultArray.push(i);
+  //     naturalNumber = naturalNumber / i;
+  //     resultArray.push(naturalNumber);
+  //     console.log(resultArray);
+
+  //   }
+  // }
+}
 
 function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+  let naturalNumber = parseInt(input.split(" "));
+  findPrimeNumber(naturalNumber);
+  result(naturalNumber);
+  
+
 }
+
+
 
 module.exports = main;
